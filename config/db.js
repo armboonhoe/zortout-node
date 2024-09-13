1135910bd6649c28b76f10119037bca95bfe59cf
armboonhoe <armboonhoe@gmail.com>
2024-09-14 01:01:52 +0700

    dasdasd

--- config/db.js
+++ config/db.js
@@ -7,8 +7,8 @@ const mongoURI = process.env.MONGO_URI;
 const connectDB = async () => {
   try {
     await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log("Connected to MongoDB")
     logger.info("Connected to MongoDB");
